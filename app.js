5d3d7214c85cd63ea77e6dd8579f660c11facaa2
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2021-10-06 08:52:32 +0200

    dodanie morgan i podstawowe style

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require ('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate');
 const Kid = require('./models/picture')
+const morgan = require('morgan');
 
 mongoose.connect('mongodb://localhost:27017/karolinav2', {
 })
@@ -20,6 +22,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'));
+app.use(morgan('tiny'));
 
 app.get('/', (req,res) => {
     res.render('home')
@@ -34,10 +38,32 @@ app.get('dzieci/new', (req,res) => {
     res.render('dzieci/index')
 })
 
+app.get('dzieci/:id', async (req,res) => {
+    const kid = await Kid.findById(req.params.id)
+    res.render('dzieci', {kid})
+})
+
+app.get('dzieci/:id/edit', async (req,res) => {
+    const kid = await Kid.findById(req.params.id)
+    res.render('dzieci/edit', {kid});
+})
+
 app.post('/dzieci', async (req,res) => {
 const kid = new Kid(req.body.kid);
 await kid.save();
 res.redirect('dzieci')
+})
+
+app.put('/dzieci', async (req, res) => {
+    const {id} = req.params
+    const kid = Kid.findByIdAndUpdate(id, {...req.body.kid});
+    res.redirect('dzieci', {kid})
+})
+
+app.delete('/dzieci/:id', async (req, res) => {
+const {id} = req.params;
+await Kid.findByIdAndDelete(id);
+res.redirect('dzieci');
 })
 
 app.listen(3000, () =>  {
